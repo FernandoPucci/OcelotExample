4aa269ba687ffed5c68c6cb22b601c6ee96383d8
Fernando Pucci <fsilvapucci@gmail.com>
2018-11-07 23:37:00 -0200

    Upgrade to Ocelot 12.0.1

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 
 namespace OcelotExample
 {
@@ -8,19 +17,34 @@ namespace OcelotExample
     {
         public static void Main(string[] args)
         {
-             IWebHostBuilder builder = new WebHostBuilder();
-
-            builder.ConfigureServices(s => {
-                s.AddSingleton(builder);
-            });
-
-            builder.UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseStartup<Startup>();
-
-            var host = builder.Build();
-
-            host.Run();
+                  new WebHostBuilder()
+            .UseKestrel()
+            .UseUrls("http://localhost:5000")
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config
+                    .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                    .AddJsonFile("appsettings.json", true, true)
+                    .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                    .AddJsonFile("ocelot.json")
+                    .AddEnvironmentVariables();
+            })
+            .ConfigureServices(s => {
+                s.AddOcelot();
+                 //   .AddAdministration("/administration", "secret");
+            })
+            .ConfigureLogging((hostingContext, logging) =>
+            {
+                logging.AddConsole();
+            })
+            .UseIISIntegration()
+            .Configure(app =>
+            {
+                app.UseOcelot().Wait();
+            })
+            .Build()
+            .Run();
         }
     }
 }
